b6c1d32fb5dd45f4a2e87cdd0cc50d8009d86f96
nguyenvietanh2003 <146902399+vietanh77@users.noreply.github.com>
2024-02-03 11:41:58 +0700

    nhanvien

--- src/main/java/com/duyhk/clothing_ecommerce/ControllerFinal/NhanVienController.java
+++ src/main/java/com/duyhk/clothing_ecommerce/ControllerFinal/NhanVienController.java
@@ -1,14 +1,32 @@
 package com.duyhk.clothing_ecommerce.ControllerFinal;
 
+import com.duyhk.clothing_ecommerce.dto.PageRequestDTO;
+import com.duyhk.clothing_ecommerce.dto.UserDTO;
+import com.duyhk.clothing_ecommerce.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/nhan-vien")
 public class NhanVienController {
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public String home(){
+    public String home(@RequestParam(required = false, name = "page") Integer page, Model model) {
+        page = page == null ? 0 : page;
+        model.addAttribute("list", userService.getByPageRequest(new PageRequestDTO(page, 5)));
         return "NhanVien/NhanVien";
+    }
+
+    @PostMapping("/action")
+    public String action(@ModelAttribute UserDTO user) {
+        if (user.getId() == null)
+            userService.create(user);
+        else
+            userService.update(user);
+        return "redirect:/nhan-vien";
     }
 }
